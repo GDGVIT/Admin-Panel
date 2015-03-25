848024eaf4e01f48e7086a11ae0355ffa855aa57
Amit Rai <amit.rai1095@gmail.com>
2015-03-25 17:21:49 +0530

    Side Navbar View Improved

--- main.php
+++ main.php
@@ -103,7 +103,7 @@ $status=$_SESSION["status"];
                   <img src="img/logo.png" style="padding-top:10px;width:180px;height:50px;">
               </a>
           </li>
-          <li style="height:20px;padding-top:60px;"></li>
+          <li style="height:20px;padding-top:80px;"></li>
           <li class="waves-effect waves-light" id="over" style="width:240px;">
               <a href="main.php" style="color:#C6C6C6;font-weight:bold;font-size:13px;">
                   Overview
